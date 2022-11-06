feda047b06e1b9f665a6421d1ca75a58e87f87bf
Dereje3030 <51210918+Dereje3030@users.noreply.github.com>
2022-11-06 12:30:52 +0300

    after adding the tab

--- js/tab_toggle.js
+++ js/tab_toggle.js
@@ -1,4 +1,4 @@
-<script type="text/javascript">
+{/* <script type="text/javascript">
 $(document).ready(function(){
     $(".tabs_list p a").click(function(e){
         e.preventDefault();
@@ -14,4 +14,30 @@ $(document).ready(function(){
      });
    
 });
-</script>
+</script> */}
+
+let toTab1 = document.getElementById('to_tab1');
+let toTab2 = document.getElementById('to_tab2');
+let tab1 = document.getElementById('tab1');
+let tab2 = document.getElementById('tab2');
+let hrTab1 = document.getElementById('hr_tab1');
+let hrTab2 = document.getElementById('hr_tab2');
+
+toTab1.addEventListener('click', () => {
+   toTab1.classList.add('active_tab');
+   toTab2.classList.remove('active_tab');
+   hrTab1.classList.remove('in_active_line');
+   hrTab2.classList.add('in_active_line');
+   tab1.classList.remove('in_active_tab_info');
+   tab2.classList.add('in_active_tab_info');
+});
+
+toTab2.addEventListener('click', () => {
+   toTab1.classList.remove('active_tab');
+   toTab2.classList.add('active_tab');
+   hrTab1.classList.add('in_active_line');
+   hrTab2.classList.remove('in_active_line');
+   tab1.classList.add('in_active_tab_info');
+   tab2.classList.remove('in_active_tab_info');
+   tab2.classList.add('course_overview_container');
+});
